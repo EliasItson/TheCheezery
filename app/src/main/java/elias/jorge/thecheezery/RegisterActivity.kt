84230d501235831bf99194aabe12f5ac0d80cb16
GitHElias <jorge.elias@potros.itson.edu.mx>
2025-02-12 13:18:43 -0700

    Small Update
    ;)

--- app/src/main/java/elias/jorge/thecheezery/RegisterActivity.kt
+++ app/src/main/java/elias/jorge/thecheezery/RegisterActivity.kt
@@ -8,22 +8,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val buttonStart = findViewById<Button>(R.id.buttonStart)
+        val buttonSignIn = findViewById<Button>(R.id.buttonSignIn)
 
-        buttonStart.setOnClickListener {
-            var intent = Intent(this, RegisterActivity::class.java)
+        buttonSignIn.setOnClickListener {
+            var intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
